1381b469b7abc9727f8826e0cd9035e023a8579b
jwawon <originjwa@gmail.com>
2019-02-08 14:12:17 +0900

    first commit & complete assetbundle setting

---
+++ Assets/Scritps/Editor/BundleBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BundleBuilder : Editor
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles(){
+        string path = EditorUtility.SaveFolderPanel("Save AssetBundles to folder", "", "");
+        //Save to C drive
+
+        // BuildPipeline.BuildAssetBundles(@"C:\Users\Jwawon\UnityProject\AssetBundles", BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
+        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
+    }
+
+
+}
---
+++ Assets/Scritps/LoadAssetBundles.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class LoadAssetBundles : MonoBehaviour
+{
+    AssetBundle myLoadedAssetbundle;
+    //Local Path: C:\Users\Jwawon\UnityProject\AssetBundles\test
+    //GCP Path: https://storage.googleapis.com/crawler_data/OctosAssetTest/1.0.1/test
+    public string path;
+    
+    public string assetName;
+    void Start()
+    {
+        // LoadAssetBundle(path);
+        // InstatiateObjectFromBundle(assetName);
+        StartCoroutine(GetAssetBundle(path));
+    }
+
+    IEnumerator GetAssetBundle(string bundleUrl){
+        using (UnityWebRequest uwr =  UnityWebRequestAssetBundle.GetAssetBundle(bundleUrl))
+        {
+            yield return uwr.SendWebRequest();
+            if(uwr.isNetworkError || uwr.isHttpError){
+                Debug.Log(uwr.error);
+            }
+            else{
+                myLoadedAssetbundle = DownloadHandlerAssetBundle.GetContent(uwr);
+                InstatiateObjectFromBundle(assetName);
+
+            }
+        }
+    }
+    void LoadAssetBundle(string bundleUrl){
+        myLoadedAssetbundle = AssetBundle.LoadFromFile(bundleUrl);
+        Debug.Log(myLoadedAssetbundle == null ? "Failed to load AssetBundle" : "AssetBundle successfully loaded");   
+    }
+
+    void InstatiateObjectFromBundle(string assetName){
+        var prefab = myLoadedAssetbundle.LoadAsset(assetName);
+        Instantiate(prefab);
+
+    }
+}
